5c2b8fc97588235fcdc4b501533666f2b965dbb8
tatikrscm-ops <tatikrscm-ops>
2026-06-21 12:27:54 +0700

    feat: integrasi rumus skoring otomatis IST modular

--- add_column.py
+++ add_column.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database import SQLALCHEMY_DATABASE_URL
+from database import DATABASE_URL
 
 # Buat engine dan koneksi langsung
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def upgrade_local_db():
